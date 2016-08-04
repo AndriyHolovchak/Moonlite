a78cee85d755606b1c3fcc3e2d869485d9210e6f
aholovchak <andriy531@gmail.com>
2016-08-04 12:54:41 +0300

    added edit book

--- src/app/Controllers/EditController.js
+++ src/app/Controllers/EditController.js
@@ -10,27 +10,52 @@
             .controller('EditController', EditController);
 
     /** @ngInject */
-    function EditController(APIS, $stateParams) {
+    function EditController(APIS, $stateParams, $state, Upload, API_ROOT_URL, Notification) {
         var vm = this;
 
-        APIS.getBookById($stateParams.id)
-                .then(function(res){
-                    console.log(res);
-                    vm.book = res.data;
+        var id = $stateParams.id;
 
+        APIS.getBookById(id)
+                .then(function(res){
+                    vm.book = res.data;
                     vm.name = vm.book.name;
-                    vm.photo = vm.book.picture.url;
-                    vm.file = vm.book.file.url;
+                    vm.existPhoto = vm.book.picture.url;
+                    vm.existFile = vm.book.file.url;
                 });
 
         vm.uploadPhoto = function (photo) {
             vm.photo = photo;
-            vm.uploadError = null;
+            vm.existPhoto = null;
         };
 
         vm.uploadFile = function (file) {
             vm.file = file;
-            vm.uploadError = null;
+            vm.existFile = null;
         };
+
+
+        vm.editBook = function(name, file, photo) {
+            vm.uploading = true;
+
+            var sendObj = {
+                'book[name]': name,
+                'book[picture]': photo,
+                'book[file]': file
+            };
+
+            Upload.upload({
+                url: API_ROOT_URL + 'books/' + id,
+                data: sendObj,
+                method: 'PUT'
+            }).then(function(res) {
+                Notification({ message: 'Your book has been uploaded' }, 'success');
+                $state.go('main.books');
+                vm.uploading = false;
+                return res;
+            }, function(error) {
+                vm.uploading = false;
+                Notification({ message: 'Something happened with your uploading' }, 'warning');
+            })
+        }
     }
 })();
